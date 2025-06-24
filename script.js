266ce9c74724f24fdcad9d9e6acefd1930e6d947
Delegates Account <delegates@lums.edu.pk>
2025-06-24 10:43:13 +0500

    Initial phase

---
+++ script.js
@@ -0,0 +1,4 @@
+// Simple interactivity for the "Contact Me" button
+document.getElementById("contactBtn").addEventListener("click", () => {
+  alert("You clicked Contact Me! You can link this to your email or contact form.");
+});
